eaa0f18d4e53e18c45cef1dd1283541611ba4382
Vladislav-creator <valdisbeketov@gmail.com>
2023-07-02 23:03:59 +0300

    еее

--- lesson-1/module.js
+++ lesson-1/module.js
@@ -1,2 +1,2 @@
 //Дана строка Выведите только числа массивом из строки(не отдельно каждую цифру, а [28, 1977])
-const str = "Vladislav Beketov was born on February 28, 1977";
+const str = "Vladislav Beketov was born on  28 February 1977";
